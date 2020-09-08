1ecbd63761de22495071bf984e53233fd8c1e7d0
kris <kristmitina@gmail.com>
2020-09-09 00:08:24 +0300

    release v1

---
+++ WeatherData.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by DVKSH on 01.09.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable{
+    let description: String
+    let id: Int
+}
